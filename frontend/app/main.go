27e9c5fe31724a5c251fc6469a74a25148f41c8f
Sergey Smolnikov <ssmol@equinor.com>
2022-01-18 13:48:38 +0100

    fix

--- frontend/app/main.go
+++ frontend/app/main.go
@@ -5,15 +5,21 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
-var envVars = map[string]bool{"CONNECTION-STRING": true, "DB_USER": true, "DB_PASS": true, "DB_QA_USER": true}
+var envVars = map[string]bool{"CONNECTION_STRING": true, "DB_USER": true, "DB_PASS": true, "DB_QA_USER": true}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Azure Key vault env-vars</h1>")
 	fmt.Fprintf(w, "<ul>")
 	for _, envVar := range os.Environ() {
-		if _, ok := envVars[envVar]; ok {
+		envVarNameValue := strings.Split(envVar, "=")
+		if len(envVarNameValue) < 2 {
+			continue
+		}
+		envVarName := envVarNameValue[0]
+		if _, ok := envVars[envVarName]; ok {
 			fmt.Fprintf(w, "<li>%s</li>", envVar)
 		}
 	}
@@ -23,6 +29,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Println("Start server")
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8082", nil))
 	fmt.Println("Stopped server")
 }
